4f989c132bbceea03ff8c6217bd04e9d6693ccc4
imaks3112 <lankeshwarakshay3112@gmail.com>
2021-06-03 14:34:07 +0530

    Task 1

---
+++ JavascriptFundamentals/1.1.js
@@ -0,0 +1,23 @@
+// alert("I'm JavaScript");
+// alert("I'm Angular");
+
+
+// let admin, name
+// let names = "John";
+
+// admin = names;
+
+// alert(admin);
+
+
+// let ourPlanetName = 'earth';
+
+// let currentUserName = "Akshay";
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); 
+
+alert( `hello ${"name"}` ); 
+
+alert( `hello ${name}` );
